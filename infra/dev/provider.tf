05ea7cdb9e6fccdafc351d9c5e68ef7e5fd8284a
Shruti Grover <shruti.grover96@gmail.com>
2025-12-28 20:15:56 +0530

    Update provider.tf

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -1,4 +1,21 @@
 provider "azurerm" {
   features {}
-  subscription_id = "6e65d60a-bc12-4f70-8dcd-6cdb4da48e3e"
+ # subscription_id = "6e65d60a-bc12-4f70-8dcd-6cdb4da48e3e"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+backend "azurerm" {
+    resource_group_name  = "shruti-rg"
+    storage_account_name = "shrutiaccount2608"
+    container_name       = "mycontainer"
+    key                  = "shruti.tfstate"
+
+  }
 }
